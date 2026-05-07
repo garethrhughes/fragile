f9a3367532499dcfcd04ec3e1473922c10d38130
Gareth Hughes <ghughes@mypassglobal.com>
2026-05-07 22:58:26 +1000

    Revert "chore: adding mypass env settings"
    This reverts commit da2ec92bf3dc769de1c6f323533728ac757d71c4.

--- infra/terraform/environments/prod/backend.tf
+++ infra/terraform/environments/prod/backend.tf
@@ -17,13 +17,14 @@
 #     --server-side-encryption-configuration \
 #       '{"Rules":[{"ApplyServerSideEncryptionByDefault":{"SSEAlgorithm":"AES256"}}]}'
 
+
 terraform {
   backend "s3" {
     # TODO: Replace with your S3 bucket name (created manually during bootstrap)
-    bucket = "mypassglobal-ops-acct-terraform"
+    bucket = "fragile-terraform-state-TODO"
 
     # State file path within the bucket
-    key = "fragile/prod/terraform.tfstate"
+    key = "prod/terraform.tfstate"
 
     # Must match the region where the bucket was created
     region = "ap-southeast-2"
